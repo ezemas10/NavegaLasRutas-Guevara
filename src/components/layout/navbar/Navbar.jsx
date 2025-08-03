608df3ba13521c60bd5dff594798d07acc0058b6
ezemas10 <ezeguevaraf@hotmail.com>
2025-08-02 23:46:03 -0300

    navbar responsive

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -7,12 +7,35 @@ export const Navbar = ({ contador }) => {
     <nav className="navbar navbar-expand-lg navbar-estilopropio fixed-top">
       <div className="container-fluid">
 
-        <a href="#"><img className="logonav" src={logoCelesteTienda} alt="Logo Consultorios Podológicos"/></a>
-
-          <div className="d-flex align-items-center gap-3">
+        <div className="d-flex align-items-center gap-3">
             <CartWidget />
             <span className="counterClass"> {contador} </span> 
           </div>
+
+        <a href="#"><img className="logonav" src={logoCelesteTienda} alt="Logo Consultorios Podológicos"/></a>
+
+        <button class="navbar-toggler navbar-buttonpropio" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon navbar-buttonpropio"></span>
+            </button>
+            <div class="collapse navbar-collapse navbar-collapse-propia" id="navbarNav">
+              <ul class="navbar-nav navegacionpropia">
+                <li class="nav-item">
+                  <a class="nav-link links-propios" href="#">Categoría 1</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link links-propios" href="#">Categoría 2</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link links-propios" href="#">Categoría 3</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link links-propios" href="#">Categoría 4</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link links-propios" href="#">Categoría 5</a>
+                </li>
+              </ul>
+            </div>
 
       </div>
     </nav>
