81d8b477cdfbde06793374a292768e87d0ec2d2b
ezemas10 <ezeguevaraf@hotmail.com>
2025-08-04 00:21:14 -0300

    Rutas Ok

--- src/components/common/productCard/ProductCard.jsx
+++ src/components/common/productCard/ProductCard.jsx
@@ -1,20 +1,27 @@
 
 import "./productCard.css";
+import { Link } from "react-router";
 
 const ProductCard = ({ product }) => {
-  const { titulo, descripcion, precio , stock, categoria, imageUrl} = product;
+  const { id, titulo, descripcion, precio , stock, categoria, imageUrl} = product;
   return (
     
     <div className="cardClass">
+      <Link to={`/detalle/${id}`} className="cardLinks">
+          <div>
 
-      <h1>{titulo}</h1>
-      <img className= "imagenes" src = {imageUrl}></img>
-      <h2>Descripción: </h2>
-      <h3>{descripcion}</h3>
-      <h2>Precio:</h2> 
-      <h3>{precio}</h3>
-      <h2>Stock: </h2>
-      <h3>{stock}</h3>
+            <h1>{titulo}</h1>
+            <img className= "imagenes" src = {imageUrl}/>
+            <h2>Descripción: </h2>
+            <h3>{descripcion}</h3>
+            <h2>Precio:</h2> 
+            <h3>{precio}</h3>
+            <h2>Stock: </h2>
+            <h3>{stock}</h3>
+
+          </div>
+
+      </Link>
 
     </div>
 
